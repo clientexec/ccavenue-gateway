cac76cd3bf64115bd45be36123c15e0df462f33c
Juan David Bolivar <juan@clientexec.com>
2014-05-20 09:37:26 -0500

    (#1185) - Updated CCAvenue gateway plugin. (JD)

--- PluginCcavenue.php
+++ PluginCcavenue.php
@@ -25,13 +25,23 @@ class PluginCcavenue extends GatewayPlugin
                                 ),
             /*T*/"Merchant ID"/*/T*/ => array (
                                 "type"          =>"text",
-                                "description"   =>/*T*/"ID used to identify you to CCAvenue."/*/T*/,
+                                "description"   =>/*T*/"This is the identifier for your CCAvenue merchant Account."/*/T*/,
                                 "value"         =>""
                                 ),
-            /*T*/"Working Key"/*/T*/ => array (
+            /*T*/"Access Code"/*/T*/ => array (
                                 "type"          =>"text",
-                                "description"   =>/*T*/"32 bit alphanumber CCAvenue key.<br>Note: This key is available at 'Generate Working Key' of the 'Settings & Options' section."/*/T*/,
+                                "description"   =>/*T*/"This is the access code for your application."/*/T*/,
                                 "value"         =>""
+                                ),
+            /*T*/"Encryption Key"/*/T*/ => array (
+                                "type"          =>"text",
+                                "description"   =>/*T*/"The secret key used for encrypting each request originating from your application.<br>Ensure you are using the correct key while encrypting requests from different URLs registered with CCAvenue."/*/T*/,
+                                "value"         =>""
+                                ),
+            /*T*/"Test Mode"/*/T*/ => array(
+                                "type"          =>"yesno",
+                                "description"   =>/*T*/"Select YES if you want to use Testing server, so no actual monetary transactions are made."/*/T*/,
+                                "value"         =>"0"
                                 ),
             /*T*/"Visa"/*/T*/ => array (
                                 "type"          =>"yesno",
@@ -84,10 +94,10 @@ class PluginCcavenue extends GatewayPlugin
                                 "value"         =>"0"
                                 ),
             /*T*/"Check CVV2"/*/T*/ => array (
-                                        "type"          =>"hidden",
-                                        "description"   =>/*T*/"Select YES if you want to accept CVV2 for this plugin."/*/T*/,
-                                        "value"         =>"0"
-                                       )
+                                "type"          =>"hidden",
+                                "description"   =>/*T*/"Select YES if you want to accept CVV2 for this plugin."/*/T*/,
+                                "value"         =>"0"
+                                )
         );
         return $variables;
     }
@@ -103,42 +113,76 @@ class PluginCcavenue extends GatewayPlugin
         //Function needs to build the url to the payment processor
         //Plugin variables can be accesses via $params["plugin_[pluginname]_[variable]"] (ex. $params["plugin_paypal_UserID"])
 
-        include("plugins/gateways/ccavenue/libfuncs.php");
+        include("plugins/gateways/ccavenue/Crypto.php");
+        $access_code = $params["plugin_ccavenue_Access Code"];
+        $WorkingKey = $params["plugin_ccavenue_Encryption Key"];
 
-        $Merchant_Id = $params["plugin_ccavenue_Merchant ID"];//This id(also User Id)  available at "Generate Working Key" of "Settings & Options"
-        $Amount = sprintf("%01.2f", round($params["invoiceTotal"], 2));//your script should substitute the amount in the quotes provided here
-        $Order_Id = $params['invoiceNumber'];//your script should substitute the order description in the quotes provided here
+        //Need to check to see if user is coming from signup
+        if ($params['isSignup']==1) {
+            // Actually handle the signup URL setting
+            if($this->settings->get('Signup Completion URL') != '') {
+                $returnURL_Cancel = $this->settings->get('Signup Completion URL');
+            }else{
+                $returnURL_Cancel = $params["clientExecURL"]."/order.php?step=3";
+            }
+        }else {
+            $returnURL_Cancel = $params["invoiceviewURLCancel"];
+        }
 
-        /**
-         * OrderID should be unique for ccavenue gateway. So we need to append timestamp with the InvoiceID like invoiceid_timestamp
-         */
+        $merchant_data = //Required Parameters: Merchant must send the following parameters to the CCAvenue PG for processing an order.
+                         'merchant_id='.urlencode($params["plugin_ccavenue_Merchant ID"])
+                        .'&order_id='.urlencode($params['invoiceNumber']."_".time()) // OrderID should be unique for ccavenue gateway. So we need to append timestamp with the InvoiceID like invoiceid_timestamp
+                        .'&currency='.urlencode($params["currencytype"])
+                        .'&amount='.urlencode(sprintf("%01.2f", round($params["invoiceTotal"], 2)))
+                        .'&redirect_url='.urlencode($params['clientExecURL']."/plugins/gateways/ccavenue/callback.php")
+                        .'&cancel_url='.urlencode($returnURL_Cancel)
+                        .'&language='.urlencode('EN')
 
-	$Order_Id = $Order_Id."_".time();
-        $Redirect_Url = $params['clientExecURL']."/plugins/gateways/ccavenue/callback.php";//your redirect URL where your customer will be redirected after authorisation from CCAvenue
-        $WorkingKey = $params["plugin_ccavenue_Working Key"];//put in the 32 bit alphanumeric key in the quotes provided here.Please note that get this key ,login to your CCAvenue merchant account and visit the "Generate Working Key" section at the "Settings & Options" page.
-        $Checksum = getCheckSum($Merchant_Id,$Amount,$Order_Id ,$Redirect_Url,$WorkingKey);
+                        //Billing Information: Merchant can send any of the following parameters in addition to the required parameters.
+                        .'&billing_name='.urlencode($params["userFirstName"]." ".$params["userLastName"])
+                        .'&billing_address='.urlencode($params["userAddress"])
+                        .'&billing_city='.urlencode($params["userCity"])
+                        .'&billing_state='.urlencode($params["userState"])
+                        .'&billing_zip='.urlencode($params["userZipcode"])
+                        .'&billing_country='.urlencode($params["userCountry"])
+                        .'&billing_tel='.urlencode($params["userPhone"])
+                        .'&billing_email='.urlencode($params["userEmail"])
+
+                        //Shipping Information: Merchant can send any of the following parameters in addition to the required parameters.
+                        .'&delivery_name='.urlencode($params["userFirstName"]." ".$params["userLastName"])
+                        .'&delivery_address='.urlencode($params["userAddress"])
+                        .'&delivery_city='.urlencode($params["userCity"])
+                        .'&delivery_state='.urlencode($params["userState"])
+                        .'&delivery_zip='.urlencode($params["userZipcode"])
+                        .'&delivery_country='.urlencode($params["userCountry"])
+                        .'&delivery_tel='.urlencode($params["userPhone"])
+
+                        .'&merchant_param1='.urlencode($params['isSignup'])
+                        .'&merchant_param2='.urlencode("Invoice #".$params['invoiceNumber'])
+                        //.'&merchant_param3='.urlencode($merchant_param3)
+                        //.'&merchant_param4='.urlencode($merchant_param4)
+                        //.'&merchant_param5='.urlencode($merchant_param5)
+
+                        //.'&promo_code='.urlencode($promo_code)
+
+                        //.'&customer_identifier='.urlencode($customer_identifier)
+                        ;
+
+        $encrypted_data = encrypt($merchant_data, $WorkingKey); // Method for encrypting the data.
+
+        if ( $params['plugin_ccavenue_Test Mode'] == '1' ) {
+            $ccavenuePost = 'test';
+        }else{
+            $ccavenuePost = 'secure';
+        }
 
         $strRet = "<html>\n";
         $strRet .= "<head></head>\n";
         $strRet .= "<body>\n";
-        $strRet .= "<form name=ccavenue method=\"post\" action=\"https://www.ccavenue.com/shopzone/cc_details.jsp\">";
-        $strRet .= "<input type=hidden name=Merchant_Id value=\"$Merchant_Id\">";
-        $strRet .= "<input type=hidden name=Amount value=\"$Amount\">";
-        $strRet .= "<input type=hidden name=Order_Id value=\"$Order_Id\">";
-        $strRet .= "<input type=hidden name=Redirect_Url value=\"$Redirect_Url\">";
-        $strRet .= "<input type=hidden name=Checksum value=\"$Checksum\">";
-
-        $strRet .= "<input type=\"hidden\" name=\"billing_cust_name\" value=\"".$params["userFirstName"]." ".$params["userLastName"]."\">";
-        $strRet .= "<input type=\"hidden\" name=\"billing_cust_address\" value=\"".$params["userAddress"]."\">";
-        $strRet .= "<input type=\"hidden\" name=\"billing_cust_country\" value=\"".$params["userCountry"]."\">";
-        $strRet .= "<input type=\"hidden\" name=\"billing_cust_tel\" value=\"".$params["userPhone"]."\">";
-        $strRet .= "<input type=\"hidden\" name=\"billing_cust_email\" value=\"".$params["userEmail"]."\">";
-        $strRet .= "<input type=\"hidden\" name=\"delivery_cust_name\" value=\"".$params["userFirstName"]." ".$params["userLastName"]."\">";
-        $strRet .= "<input type=\"hidden\" name=\"delivery_cust_address\" value=\"".$params["userAddress"]."\">";
-        $strRet .= "<input type=\"hidden\" name=\"delivery_cust_tel\" value=\"".$params["userPhone"]."\">";
-        $strRet .= "<input type=\"hidden\" name=\"delivery_cust_notes\" value=\"Invoice #".$Order_Id."\">";
-        $strRet .= "<input type=\"hidden\" name=\"Merchant_Param\" value=\"".$params['isSignup']."\">";
-
+        $strRet .= "<form name=ccavenue method=\"post\" action=\"https://".$ccavenuePost.".ccavenue.com/transaction/transaction.do?command=initiateTransaction\">";
+        //$strRet .= "<input type=hidden name=command value=initiateTransaction>";
+        $strRet .= "<input type=hidden name=encRequest value=".$encrypted_data.">";
+        $strRet .= "<input type=hidden name=access_code value=".$access_code.">";
         $strRet .= "<script language=\"JavaScript\">\n";
         $strRet .= "document.forms['ccavenue'].submit();\n";
         $strRet .= "</script>\n";
